38ed6314bc7cf83c648e72ac500acb3b48ed1840
Mateusz Bysiek <mb@mbdev.pl>
2011-12-18 02:40:28 +0100

    less static
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- FileSyncWcfService/EntityFramework/UserManipulator.cs
+++ FileSyncWcfService/EntityFramework/UserManipulator.cs
@@ -7,170 +7,73 @@ using System.Data.Objects.DataClasses;
 
 using FileSyncObjects;
 
-namespace FileSyncWcfService.EntityFramework
-{
-    /// <summary>
-    /// Class used to managing Users table
-    /// Allows to:
-    /// - add a new user
-    /// - log in
-    /// - check if a given username is available or occupied
-    /// - delete a user
-    /// - update information about last login
-    /// - authenticate (check wheather a passwd maches login)
-    /// </summary>
-    public class UserManipulator
-    {
-        public static void DelUser(CredentialsLib c)
-        {
-            UserModel u = GetUser(c);
+namespace FileSyncWcfService.EntityFramework {
+	/// <summary>
+	/// Class used to managing Users table
+	/// Allows to:
+	/// - add a new user
+	/// - log in
+	/// - check if a given username is available or occupied
+	/// - delete a user
+	/// - update information about last login
+	/// - authenticate (check wheather a passwd maches login)
+	/// </summary>
+	public class UserManipulator {
 
-            using (filesyncEntities context = new filesyncEntities())
-            {
-                try
-                {
-                    int id = LoginToId(u.Login);
-                    var u1 = (from o in context.Users
-                              where o.user_id == id
-                              select o).Single();
-                    context.Users.DeleteObject(u1);
-                    context.SaveChanges();
-                }
-                catch
-                {
-                    throw new Exception("no such user");
-                }
+		//public static void DelUser(CredentialsLib c) {
+		//    UserModel u = GetUser(c);
+
+		//    using (filesyncEntities context = new filesyncEntities()) {
+		//        try {
+		//            int id = LoginToId(u.Login);
+		//            var u1 = (from o in context.Users
+		//                      where o.user_id == id
+		//                      select o).Single();
+		//            context.Users.DeleteObject(u1);
+		//            context.SaveChanges();
+		//        } catch {
+		//            throw new Exception("no such user");
+		//        }
 
 
-            }
-        }
-        private static void UpdateLastLogin(int id)
-        {
+		//    }
+		//}
+
+		public static void UpdateLastLogin(int id) {
+
+			using (filesyncEntities context = new filesyncEntities()) {
+				try {
+					var u1 = (from o in context.Users
+							  where o.user_id == id
+							  select o).Single();
+					u1.user_lastlogin = DateTime.Now;
+					context.SaveChanges();
+				} catch {
+					throw new Exception("no such user");
+				}
+			}
+		}
+		public static bool LoginExists(string login) {
 
 
-            using (filesyncEntities context = new filesyncEntities())
-            {
-                try
-                {
-                    var u1 = (from o in context.Users
-                              where o.user_id == id
-                              select o).Single();
-                    u1.user_lastlogin = DateTime.Now;
-                    context.SaveChanges();
-                }
-                catch
-                {
-                    throw new Exception("no such user");
-                }
+			using (filesyncEntities context = new filesyncEntities()) {
+				User u1;
+				try {
 
 
-            }
-
-        }
-
-        public static bool LoginIn(CredentialsLib c)
-        {
+					u1 = (from o in context.Users
+						  where o.user_login == login
+						  select o).Single();
 
 
-            using (filesyncEntities context = new filesyncEntities())
-            {
-                User u1;
-                try
-                {
+				} catch {
+					return false;
+				}
 
+				return true;
+			}
 
-                    u1 = (from u in context.Users
-                          where u.user_login == c.Login && u.user_pass == c.Pass
-                          select u).Single();
-                    UpdateLastLogin(LoginToId(c.Login));
+		}
 
-
-                }
-                catch
-                {
-                    return false;
-                }
-
-                return true;
-            }
-
-        }
-        public static bool Authenticate(CredentialsLib c)
-        {
-
-
-            using (filesyncEntities context = new filesyncEntities())
-            {
-                User u1;
-                try
-                {
-
-
-                    u1 = (from u in context.Users
-                          where u.user_login == c.Login && u.user_pass == c.Pass
-                          select u).Single();
-
-
-
-                }
-                catch
-                {
-                    return false;
-                }
-
-                return true;
-            }
-
-        }
-        public static bool LoginExists(string login)
-        {
-
-
-            using (filesyncEntities context = new filesyncEntities())
-            {
-                User u1;
-                try
-                {
-
-
-                    u1 = (from o in context.Users
-                          where o.user_login == login
-                          select o).Single();
-
-
-                }
-                catch
-                {
-                    return false;
-                }
-
-                return true;
-            }
-
-        }
-        internal static int LoginToId(string login)
-        {
-            if (LoginExists(login))
-            {
-
-                using (filesyncEntities context = new filesyncEntities())
-                {
-
-                    User u1 = (from o in context.Users
-                               where o.user_login == login
-                               select o).Single();
-
-                    return u1.user_id;
-                }
-            }
-            else
-            {
-
-                throw new Exception("no such user");
-
-            }
-
-        }
-
-    }
+	}
 }
--- FileSyncWcfService/FileSyncService.svc.cs
+++ FileSyncWcfService/FileSyncService.svc.cs
@@ -9,12 +9,11 @@ using System.Text;
 using FileSyncObjects;
 using FileSyncWcfService.EntityFramework;
 
-namespace WcfServiceTest
-{
-    /// <summary>
-    /// Implementation of FileSync interface, which connects directly to the database.
-    /// </summary>
-    public class FileSyncService : IFileSyncModel {
+namespace WcfServiceTest {
+	/// <summary>
+	/// Implementation of FileSync interface, which connects directly to the database.
+	/// </summary>
+	public class FileSyncService : IFileSyncModel {
 
 		#region User
 
@@ -36,7 +35,19 @@ namespace WcfServiceTest
 		}
 
 		public void Login(Credentials c) {
-			throw new Exception("not implemented");
+			using (filesyncEntities context = new filesyncEntities()) {
+				User u1;
+				try {
+					u1 = (from u in context.Users
+						  where u.user_login == c.Login && u.user_pass == c.Pass
+						  select u).Single();
+					UserManipulator.UpdateLastLogin(LoginToId(c.Login));
+
+
+				} catch {
+					throw new Exception("Authorization error.");
+				}
+			}
 		}
 
 		public UserContents GetUser(Credentials c) {
@@ -63,6 +74,32 @@ namespace WcfServiceTest
 
 		public void GetMachineList(Credentials c, UserContents u) {
 			throw new Exception("not implemented");
+		}
+
+		private int LoginToId(string login) {
+			if (UserManipulator.LoginExists(login)) {
+				using (filesyncEntities context = new filesyncEntities()) {
+					User u1 = (from o in context.Users
+							   where o.user_login == login
+							   select o).Single();
+					return u1.user_id;
+				}
+			} else {
+				throw new Exception("no such user");
+			}
+		}
+
+		private bool Authenticate(Credentials c) {
+			using (filesyncEntities context = new filesyncEntities()) {
+				User u1;
+				try {
+					//u1 = (from u in context.Users where u.user_login == c.Login && u.user_pass == c.Pass
+					//      select u).Single();
+				} catch {
+					return false;
+				}
+				return true;
+			}
 		}
 
 		#endregion
@@ -110,72 +147,72 @@ namespace WcfServiceTest
 
 		#endregion
 
-        //public void AddDirectory(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d)
-        //{
-        //    DirManipulator.AddDirectory(c, m, d);
-        //}
-        //public void GetDirList(RemoteMachineModel m)
-        //{
-        //    DirManipulator.GetDirList(m);
-        //}
-        //public void AddFile(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d, RemoteFileModel f)
-        //{
-        //    FileManipulator.AddFile(c, m, d, f);
-        //}
-        //public void GetFileList(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d)
-        //{
-        //    FileManipulator.GetFileList(c, m, d);
-        //}
-        //public void GetFileContent(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d, RemoteFileModel f)
-        //{
-        //    FileManipulator.GetFileContent(c, m, d, f);
-        //}
-        //public void AddMachdir(RemoteMachdirModel md)
-        //{
-        //    MachDirManipulator.Add(md);
-        //}
-        //public void AddMachine(RemoteCredentialsLib c, RemoteMachineModel m)
-        //{
-        //    MachManipulator.AddMachine(c, m);
-        //}
-        //public void GetMachineList(RemoteUserModel user)
-        //{
-        //    MachManipulator.GetMachineList(user);
-        //}
-        //public void ChangeMachineDetails(RemoteCredentialsLib c, RemoteMachineModel newMachine, RemoteMachineModel oldMachine)
-        //{
-        //    MachManipulator.ChangeMachineDetails(c, newMachine, oldMachine);
-        //}
-        //public void AddType(RemoteFileModel f)
-        //{
-        //    TypeManipulator.AddType(f);
-        //}
-        //public List<string> GetTypeList()
-        //{
-        //    return TypeManipulator.GetTypeList();
-        //}
-        //public RemoteUserModel GetUser(RemoteCredentialsLib c)
-        //{
-        //    var u = UserManipulator.GetUser(c);
-        //    var u1 = new RemoteUserModel();
-        //    u1.Login = u.Login;
-        //    u1.Pass = u.Pass;
-        //    u1.Fullname = u.Fullname;
-        //    u1.Email = u.Email;
-        //    u1.Machines = u.Machines;
-        //    u1.Id = u.Id;
-        //    u1.Lastlogin = u.Lastlogin;
-        //    return u1;
+		//public void AddDirectory(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d)
+		//{
+		//    DirManipulator.AddDirectory(c, m, d);
+		//}
+		//public void GetDirList(RemoteMachineModel m)
+		//{
+		//    DirManipulator.GetDirList(m);
+		//}
+		//public void AddFile(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d, RemoteFileModel f)
+		//{
+		//    FileManipulator.AddFile(c, m, d, f);
+		//}
+		//public void GetFileList(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d)
+		//{
+		//    FileManipulator.GetFileList(c, m, d);
+		//}
+		//public void GetFileContent(RemoteCredentialsLib c, RemoteMachineModel m, RemoteDirModel d, RemoteFileModel f)
+		//{
+		//    FileManipulator.GetFileContent(c, m, d, f);
+		//}
+		//public void AddMachdir(RemoteMachdirModel md)
+		//{
+		//    MachDirManipulator.Add(md);
+		//}
+		//public void AddMachine(RemoteCredentialsLib c, RemoteMachineModel m)
+		//{
+		//    MachManipulator.AddMachine(c, m);
+		//}
+		//public void GetMachineList(RemoteUserModel user)
+		//{
+		//    MachManipulator.GetMachineList(user);
+		//}
+		//public void ChangeMachineDetails(RemoteCredentialsLib c, RemoteMachineModel newMachine, RemoteMachineModel oldMachine)
+		//{
+		//    MachManipulator.ChangeMachineDetails(c, newMachine, oldMachine);
+		//}
+		//public void AddType(RemoteFileModel f)
+		//{
+		//    TypeManipulator.AddType(f);
+		//}
+		//public List<string> GetTypeList()
+		//{
+		//    return TypeManipulator.GetTypeList();
+		//}
+		//public RemoteUserModel GetUser(RemoteCredentialsLib c)
+		//{
+		//    var u = UserManipulator.GetUser(c);
+		//    var u1 = new RemoteUserModel();
+		//    u1.Login = u.Login;
+		//    u1.Pass = u.Pass;
+		//    u1.Fullname = u.Fullname;
+		//    u1.Email = u.Email;
+		//    u1.Machines = u.Machines;
+		//    u1.Id = u.Id;
+		//    u1.Lastlogin = u.Lastlogin;
+		//    return u1;
 
-        //}
-        //public void AddUser(RemoteUserModel u)
-        //{
-        //    UserManipulator.Add(u);
-        //}
-        //public bool LoginIn(RemoteCredentialsLib c)
-        //{
-        //    return UserManipulator.LoginIn(c);
-        //}
+		//}
+		//public void AddUser(RemoteUserModel u)
+		//{
+		//    UserManipulator.Add(u);
+		//}
+		//public bool LoginIn(RemoteCredentialsLib c)
+		//{
+		//    return UserManipulator.LoginIn(c);
+		//}
 
 		//public void AddDirectory(CredentialsLib c, MachineModel m, DirModel d)
 		//{
@@ -233,7 +270,7 @@ namespace WcfServiceTest
 		//    u1.Id = u.Id;
 		//    u1.Lastlogin = u.Lastlogin;
 		//    return u1;
-            
+
 		//}
 		//public void AddUser(UserModel u)
 		//{
@@ -243,10 +280,10 @@ namespace WcfServiceTest
 		//{
 		//    return UserManipulator.LoginIn(c);
 		//}
-        //public bool Authenticate(CredentialsLib c)
-        //{
-        //    return UserManipulator.Authenticate(c);
-        //}
-    }
+		//public bool Authenticate(CredentialsLib c)
+		//{
+		//    return UserManipulator.Authenticate(c);
+		//}
+	}
 
 }
